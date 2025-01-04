63bcdebcb26f2fb6744479b3498ec7704b98503f
Emmanuel DeVries <emmanueljdevries@gmail.com>
2025-01-04 10:46:53 -0500

    migrate to selectolax HTMLParser

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask
-#from flask import render_template
 import urllib3
+from selectolax.parser import HTMLParser
 
 app = Flask(__name__)
 
-# https://stackoverflow.com/questions/38724132/remove-html-block-in-python
-# this is brittle ... maybe use a real parser
-def removeOneTag(text, tag):
-    if "<"+tag+">" in text:
-        return text[:text.find("<"+tag+">")] + text[text.find("</"+tag+">") + len(tag)+3:]
-    else:
-        return text[:text.find("<"+tag+" ")] + text[text.find("</"+tag+">") + len(tag)+3:]
+def modify_html(text, domain):
+    tree = HTMLParser(text)
+    tree.css_first('video').decompose()
+    return tree.html
 
 
 @app.route("/<target>")
 def hello_world(target=None):
-    #return render_template("index.html")
-    #return "<p>Hello, World!</p>"
     print(target)
 
     # should handle exception
     resp = urllib3.request("GET", "https://"+target)
     
-    return (removeOneTag(resp.data.decode(), "video"), resp.status)
+    return (modify_html(resp.data.decode(), target), resp.status)
