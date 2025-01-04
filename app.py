d87cb75dec47743691e5bebd521cb2c448f7e214
Emmanuel DeVries <emmanueljdevries@gmail.com>
2025-01-04 11:15:19 -0500

    fix relative links that start with /

--- app.py
+++ app.py
@@ -4,16 +4,26 @@ from selectolax.parser import HTMLParser
 
 app = Flask(__name__)
 
-def modify_html(text, domain):
+def modify_html(text, target):
     tree = HTMLParser(text)
+    
     tree.css_first('video').decompose()
+    
+    for node in tree.css('a'):
+        if 'href' in node.attrs:
+            if node.attrs['href'] == '/':
+                node.attrs['href'] = '/'+target
+            # relative links
+            elif node.attrs['href'].startswith('/'):
+                node.attrs['href'] = "https://"+target + node.attrs['href']
+    
     return tree.html
 
 
 @app.route("/<target>")
 def hello_world(target=None):
     print(target)
-
+    
     # should handle exception
     resp = urllib3.request("GET", "https://"+target)
     
